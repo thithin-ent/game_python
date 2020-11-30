3165ebf5768edae51029fbec65808f7d50b6322e
thithin-ent <65296325+thithin-ent@users.noreply.github.com>
2020-11-30 18:15:36 +0900

    Update 1.py

--- 1.py
+++ 1.py
@@ -9,7 +9,6 @@ class draw:
         self.__h = h
         self.__id = canvas.create_rectangle(self.__x,self.__y,self.__x+self.__w,self.__y+self.__h,fill="white")
         
-        
     def setdraw(self,x,y,w,h):
         canvas.delete(self.__id)
         self.__x = x
@@ -26,10 +25,13 @@ class draw:
     
 game = Tk()
 game.title("게임")
-canvas = Canvas(game,width=1000,heigh=900,bg="black")
+width=1000
+heigh=900
+canvas = Canvas(game,width=width,heigh=heigh,bg="black")
 canvas.pack()
 move_set = set()
 
+mx,my=0,0
 test = draw(500,500,100,100)
 
 def move(event):
@@ -40,20 +42,26 @@ def move_del(event):
 
 def drawing(event):
     test.setdraw(0,0,100,100)
-
+    
+def mouseclick(event):
+    mx,my=event.x,event.y
+    if event.x > canvas.coords(test.getid())[0] and event.y > canvas.coords(test.getid())[1] and event.x < canvas.coords(test.getid())[2] and event.y < canvas.coords(test.getid())[3]:
+        test.setdraw(0,0,100,100)
+    
 canvas.bind_all("<KeyPress>", move)
 canvas.bind_all("<KeyRelease>", move_del)
+canvas.bind("<Button-1>",mouseclick)
 
 while(1):
     for i in move_set:
-        if i == "Up":
+        if i == "Up" and canvas.coords(test.getid())[1]>0:
             canvas.move(test.getid(),0,-5)
-        if i == "Down":
+        if i == "Down" and canvas.coords(test.getid())[3]<heigh:
             canvas.move(test.getid(),0,5)
-        if i == "Left":
+        if i == "Left" and canvas.coords(test.getid())[0]>0:
             canvas.move(test.getid(),-5,0)
-        if i == "Right":
+        if i == "Right" and canvas.coords(test.getid())[2]<width:
             canvas.move(test.getid(),5,0)
-
+    
     game.update()
     time.sleep(0.01)
